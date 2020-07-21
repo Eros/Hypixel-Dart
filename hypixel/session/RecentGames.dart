a318ba055761bad22bb2a39e49e7bee2181d47a3
gp2-marr <george.marr.cobc@gmail.com>
2020-07-21 22:59:42 +0100

    and more stuff

--- hypixel/session/RecentGames.dart
+++ hypixel/session/RecentGames.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 
 import '../HypixelAPI.dart';
 
-class News {
+class RecentGames {
 
-  News();
+  RecentGames();
 
-  String getNewsData() {
-    HttpClient().getUrl(Uri.parse('${HypixelAPI.BASE}news?key=${HypixelAPI.API_KEY}'))
+  String getRecentGames([String name, String uuid]) {
+    HttpClient().getUrl(Uri.parse('${HypixelAPI.BASE}recentgames?key=${HypixelAPI.API_KEY}&uuid=${uuid}'))
         .then((request) => request.close())
         .then((response) => response.transform(Utf8Decoder()).listen((e) {
       return e.toString();
--- hypixel/session/Session.dart
+++ hypixel/session/Session.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 
 import '../HypixelAPI.dart';
 
-class News {
+class Session {
 
-  News();
+  Session();
 
-  String getNewsData() {
-    HttpClient().getUrl(Uri.parse('${HypixelAPI.BASE}news?key=${HypixelAPI.API_KEY}'))
+  String getSessionData([String name, String uuid]) {
+    HttpClient().getUrl(Uri.parse('${HypixelAPI.BASE}status?key=${HypixelAPI.API_KEY}&uuid=${uuid}'))
         .then((request) => request.close())
         .then((response) => response.transform(Utf8Decoder()).listen((e) {
       return e.toString();
