fda8ec2ac9f4883c41a62bf2ce434fcdaf839519
gp2-marr <george.marr.cobc@gmail.com>
2020-07-24 17:39:50 +0100

    lets rewrite this because it's dreadful

--- hypixel/tests/main.dart
+++ hypixel/tests/main.dart
@@ -1,22 +1,4 @@
 
-import '../HypixelAPI.dart';
-import '../player/Friends.dart';
-import '../player/Player.dart';
-
-
 void main() {
-
-  HypixelAPI api = HypixelAPI();
-
-  Player player = Player();
-  Friends friends = Friends();
-
-  String testPlayerGeneral = player.getPlayerData("43db704e10b140b3a38dce059de35a59");
-  String testFriendsGeneral = friends.getPlayerFriends("43db704e10b140b3a38dce059de35a59");
-
-  api.setApiKey("REPLACE WITH YOUR API KEY");
-
-  print(testPlayerGeneral);
-  print(testFriendsGeneral);
 
 }
