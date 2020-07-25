20bca7921576a6099264fc05637cbd18f697920a
gp2-marr <george.marr.cobc@gmail.com>
2020-07-25 02:22:52 +0100

    finish up RecentGames

---
+++ hypixel/player/RecentGames.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import '../HypixelAPI.dart';
+import '../utils/ConverterUtils.dart';
+
+class RecentGames {
+
+  RecentGames();
+  ConverterUtils utils = ConverterUtils();
+
+  var url = null;
+  var response = null;
+
+  Future<String> getByUuid(String uuid) async {
+    url = HypixelAPI.BASE + 'recentgames?key=${HypixelAPI.API_KEY}&uuid=${uuid}';
+    response = await http.get(url);
+    return response.body;
+  }
+
+  Future<String> getByName(String name) async {
+    return getByUuid(utils.convertNameToUuid(name).toString());
+  }
+
+}
