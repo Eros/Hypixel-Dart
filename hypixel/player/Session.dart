4b54a292fbe8548b55d5a4297973cf9a55bbf541
gp2-marr <george.marr.cobc@gmail.com>
2020-07-25 02:20:44 +0100

    finish up session

--- hypixel/player/Session.dart
+++ hypixel/player/Session.dart
@@ -1,6 +1,23 @@
 import 'package:http/http.dart' as http;
 import '../HypixelAPI.dart';
+import '../utils/ConverterUtils.dart';
 
 class Session {
+
+  Session();
+  ConverterUtils utils = ConverterUtils();
+
+  var url = null;
+  var response = null;
+
+  Future<String> getSessionByUuid(String uuid) async {
+    url = HypixelAPI.BASE + 'status?key=${HypixelAPI.API_KEY}&uuid=${uuid}';
+    response = await http.get(url);
+    return response.body;
+  }
+
+  Future<String> getSessionByName(String name) async {
+    return getSessionByUuid(utils.convertNameToUuid(name).toString());
+  }
 
 }
