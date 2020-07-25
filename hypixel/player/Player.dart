427fd9773346aba6616551ce1bf286deffbe0361
gp2-marr <george.marr.cobc@gmail.com>
2020-07-25 01:45:21 +0100

    remove debug

--- hypixel/player/Player.dart
+++ hypixel/player/Player.dart
@@ -10,11 +10,10 @@ class Player {
   Future<String> getByUuid(String uuid) async {
     url = HypixelAPI.BASE + 'player?key=${HypixelAPI.API_KEY}&uuid=${uuid}';
     response = await http.get(url);
-    print(response.body);
     return response.body;
   }
 
   Future<String> getByName(String name) async {
-    
+
   }
 }
--- hypixel/utils/ConverterUtils.dart
+++ hypixel/utils/ConverterUtils.dart
@@ -9,11 +9,7 @@ class ConverterUtils {
   Future<String> convertNameToUuid(String name) async {
     String url = 'https://api.mojang.com/users/profiles/minecraft/${name}';
     var response = await http.get(url);
-    print(response.body);
-
     Map<String, dynamic> returnData = jsonDecode(response.body);
-
-    print(returnData['id']);
     return returnData['id'];
   }
 }
