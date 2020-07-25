b4e781f6bb6b1f9717ba3978c75feec6a7fa1482
gp2-marr <george.marr.cobc@gmail.com>
2020-07-25 01:57:40 +0100

    add in leaderboard

---
+++ hypixel/player/Leaderboard.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+import '../HypixelAPI.dart';
+
+class Leaderboard {
+
+  Leaderboard();
+
+  Future<String> getLeaderboards() async {
+    var url = HypixelAPI.BASE +'leaderboards?key=${HypixelAPI.API_KEY}';
+    var response = await http.get(url);
+    return response.body;
+  }
+}
