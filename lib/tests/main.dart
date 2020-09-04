23d57961bf2c5e2b513f430bd5943ab83f9b3ae6
Eros <george.marr.cobc@gmail.com>
2020-09-04 18:40:07 +0100

    confirm everything still works

--- lib/tests/main.dart
+++ lib/tests/main.dart
@@ -7,13 +7,10 @@ import '../utils/ConverterUtils.dart';
 void main() async {
 
   HypixelAPI api = HypixelAPI();
-  /*
-  NOTE:
 
-  This key is no longer valid nor working and has since been replaced
-  it's primary purpose was for testing.
-   */
-  api.setApiKey("6cba0922-187d-4b38-84eb-10d292cbe0d5");
+  //NOTE: any key in other commits are no longer working
+  //my normal key is no longer within this code
+  api.setApiKey("");
 
   Player player = Player();
   Leaderboard leaderboard = Leaderboard();
